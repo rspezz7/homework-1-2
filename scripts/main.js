7e641b1afaa855f7be0f42196195eedfc2701e83
spezzaferro <rachelspezz11@gmail.com>
2024-01-26 12:16:39 -0500

    submission commit

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/gilligan.jpg') {
+    myImage.setAttribute ('src','images/gilligan2.jpg');
+  } else {
+    myImage.setAttribute ('src','images/gilligan.jpg');
+  }
+};
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h2');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Welcome to my page, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome to my page, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
